325632f3dea5cf2a7a58ee9f5efebe4dbd3344a4
GersonESS <gebhsantos@gmail.com>
2025-12-31 18:03:17 -0300

    z

--- 01/backend/api-node-jwt/routes/publics.js
+++ 01/backend/api-node-jwt/routes/publics.js
@@ -1,16 +1,27 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
 const router = express.Router();
 const prisma = new PrismaClient();
 
+// Cadastro
 
-router.post("/cadastro", (req, res) => {
+router.post("/cadastro", async(req, res) => 
+{
     const user = req.body;
+    await prisma.user.create
+    {
+        data: 
+        {
+            name: user.name,
+            email: user.email,
+        
+        },
+    }
 
-    res.status(201).json({
-        message: "User registered successfully",
-        user: user,
-    });
-});
+    res.status(201).json.user
+}
+
 
 export default router;
